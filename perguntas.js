9d34a86d4d6a3384685966a7f7bd0e34d829e695
raquelyy <164918770+raquelyy@users.noreply.github.com>
2024-11-11 14:14:42 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação',
+   'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação',
+)
+
+criaCartao(
+    'Georafia',
+   'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+
+criaCartao(
+    'Programação',
+   'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa',
+)
+

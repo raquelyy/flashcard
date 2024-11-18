d88d342f696b78ae1b0a7f42251ac6f49eb6946f
raquelyy <164918770+raquelyy@users.noreply.github.com>
2024-11-18 13:52:33 -0300

    article

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ function CriaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+<div class="cartao__conteudo">
+    <h3>programação</h3>
+    <div class="cartao__conteudo__pergunta">
+         <p> O que é JavaScript?</p>
+     </div>
+     <div class="cartao__conteudo__resposta">
+       <p> O JavaScript é uma linguagem de programação.</p>
+    </div>
+   </div>
+    `
+    container.appendChild(cartao)
     
 }
